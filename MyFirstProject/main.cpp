69b3c7ca4c5df49e38c91b842ffb3611b3bd312b
dean52ICCC <dean52@tritons.iowacentral.edu>
2023-09-11 10:51:21 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,17 @@
+/*
+	Brayden Dean
+	C++ 2023
+	Due: 9/15/2023
+	Lab 1
+	Create a program that will output "Hello World!" and your favorite color and create a
+	repository on GitHub.
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Blue" << std::endl;
+
+	return(0);
+}
