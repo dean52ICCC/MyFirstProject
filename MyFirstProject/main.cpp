dc981dead71818da471911abf9a383ea3806868f
dean52ICCC <dean52@tritons.iowacentral.edu>
2023-09-11 10:51:21 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,17 @@
+/*
+	Brayden Dean
+	C++ 2023
+	Due: 9/15/2023
+
+
+
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Blue" << std::endl;
+
+	return(0);
+}
